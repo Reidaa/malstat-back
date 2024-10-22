e997eec2ae369c9b6af3d8e734676bb5213c20b2
Thomas Kemkemian <thomas.kemkemian@gmail.com>
2024-10-23 01:07:44 +0200

    remove commented code

--- main.go
+++ main.go
@@ -7,20 +7,6 @@ import (
 // Build is the last GIT commit
 var Build string
 
-// func db(connectionString string) (*gorm.DB, error) {
-// 	sqlDB, err := sql.Open("pgx", connectionString)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	gormDB, err := gorm.Open(postgres.New(postgres.Config{
-// 		Conn: sqlDB,
-// 	}), &gorm.Config{})
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	return gormDB, nil
-// }
-
 func main() {
 	cmd.Release.Build = Build
 	cmd.Run()
